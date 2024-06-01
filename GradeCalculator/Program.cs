6d5d5eb2bd60a0fd406ff171a0e8ecf7031bcc4e
BrainyYawson <acourageyawson369@gmail.com>
2024-06-01 09:34:08 +0000

    i added the grade calculator question

--- GradeCalculator/Program.cs
+++ GradeCalculator/Program.cs
@@ -8,6 +8,16 @@ namespace GradeCalculator
     {
         static void Main(string[] args)
         {
+            /*Grade Calculator
+            Write a program that prompts the user to enter a numerical grade between 0 and 100. Based
+            on the grade entered, display the corresponding letter grade using the following scale:
+            - 90 and above: A
+            - 80-89: B
+            - 70-79: C
+            - 60-69: D
+            - Below 60: F
+            */
+
             //Recieving number imput from the user
             Console.WriteLine("Enter a numerical grade between 0 and 100: ");
 
